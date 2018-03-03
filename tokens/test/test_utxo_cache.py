4be32b9931ac06d89ea9977a564cece46ea32ec2
kdenhartog <kdenhar@gmail.com>
2018-03-02 19:35:25 -0700

    fixed imports

--- tokens/test/test_utxo_cache.py
+++ tokens/test/test_utxo_cache.py
@@ -6,8 +6,8 @@ from collections import defaultdict
 import pytest
 
 from plenum.common.util import randomString
-from plenum.server.plugin.token.types import Output
-from plenum.server.plugin.token.utxo_cache import UTXOCache
+from tokens.src.types import Output
+from tokens.src.utxo_cache import UTXOCache
 from storage.test.conftest import parametrised_storage
 
 # Test Constants
