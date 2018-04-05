0c0ed534e9f6cc1db79046c3ff33599661da57da
Darien Hess <darienhess@Dariens-MacBook-Pro.local>
2018-04-05 10:42:02 -0600

    Update test_demo for passing tests

--- token/test/test_demo.py
+++ token/test/test_demo.py
@@ -2,6 +2,8 @@ from ledger.util import F
 
 
 import pytest
+
+from plenum.server.plugin.token.src.util import update_token_wallet_with_result
 from plenum.server.plugin.token.test.helper import do_public_minting, send_xfer, check_output_val_on_all_nodes, send_get_utxo
 
 from .demo.demo_helpers import *
@@ -17,42 +19,45 @@ wallets = {wallet_name: create_wallet_with_default_identifier(wallet_name) for w
 addresses = {wallet_name:create_address_add_wallet_log(wallet) for wallet_name,wallet in wallets.items()}
 
 
-@pytest.fixture(scope='session', autouse=True)
-def setup_teardown():
+@pytest.fixture(scope='module', autouse=True)
+def setup_teardown(looper, sdk_pool_handle, sdk_wallet_client, trustee_wallets):
     demo_logger.log_green('Starting test\n')
+    resp = public_mint(looper, sdk_pool_handle, sdk_wallet_client, trustee_wallets)
+    xfer_partial_amount(looper, sdk_pool_handle, resp[F.seqNo.name])
     yield
     demo_logger.log_green('Test ended')
 
 
-@pytest.fixture(scope='module')
-def public_mint(trustee_wallets, looper, txnPoolNodeSet, client1, client1Connected):
-    resp = do_public_minting(looper, trustee_wallets, client1, NEW_TOKENS,
+def public_mint(looper, sdk_pool_handle, sdk_wallet_client, trustee_wallets):
+    demo_logger.log_blue("Minting {:,} tokens.".format(NEW_TOKENS))
+
+    resp = do_public_minting(looper, trustee_wallets, sdk_pool_handle, NEW_TOKENS,
                              SF_RECEIVES, addresses["SF"], addresses["Sender"])
-    demo_logger.log_blue("You have minted {:,} tokens.".format(NEW_TOKENS))
+
     demo_logger.log_blue("The seller address, {} received {:,} tokens.".format(addresses["Sender"], SELLER_RECEIVES))
     demo_logger.log_blue("The SF address, {} received {:,} tokens".format(addresses["SF"], SF_RECEIVES))
-    for wallet in wallets.values():
-        wallet._update_outputs(resp['outputs'], resp['seqNo'])
+
+    for wallet, address in zip(wallets.values(), addresses.values()):
+        utxo_resp = send_get_utxo(looper, address, sdk_wallet_client, sdk_pool_handle)
+        update_token_wallet_with_result(wallet, utxo_resp)
     return resp
 
 
-@pytest.fixture(scope='module')
-def xfer_partial(public_mint, looper, client1, txnPoolNodeSet):
-    seq_no = public_mint[F.seqNo.name]
-    inputs = [[wallets["Sender"], addresses["Sender"], seq_no]]
+def xfer_partial_amount(looper, sdk_pool_handle, seqNo):
+    inputs = [[wallets["Sender"], addresses["Sender"], seqNo]]
     outputs = [[addresses["Sender"], SELLER_RECEIVES - RECIPIENT_RECEIVES], [addresses["Recipient"], RECIPIENT_RECEIVES]]
+
     demo_logger.log_blue("The Sender is sending {} tokens ot the Recipient".format(RECIPIENT_RECEIVES))
-    req = send_xfer(looper, inputs, outputs, client1)
-    check_output_val_on_all_nodes(txnPoolNodeSet, addresses["Sender"], SELLER_RECEIVES - RECIPIENT_RECEIVES)
-    check_output_val_on_all_nodes(txnPoolNodeSet, addresses["Recipient"], RECIPIENT_RECEIVES)
-    result, _ = client1.getReply(req.identifier, req.reqId)
-    return result
 
-
-def test_demo(xfer_partial):
+    resp = send_xfer(looper, inputs, outputs, sdk_pool_handle)
     for wallet in wallets.values():
-        wallet.handle_xfer(xfer_partial)
+        update_token_wallet_with_result(wallet, resp)
 
+
+def test_addresses_on_nodes(nodeSetWithIntegratedTokenPlugin):
+    check_output_val_on_all_nodes(nodeSetWithIntegratedTokenPlugin,addresses["Sender"], SELLER_RECEIVES - RECIPIENT_RECEIVES)
+    check_output_val_on_all_nodes(nodeSetWithIntegratedTokenPlugin, addresses["Recipient"], RECIPIENT_RECEIVES)
+    check_output_val_on_all_nodes(nodeSetWithIntegratedTokenPlugin, addresses["SF"], SF_RECEIVES)
 
 def test_sender_wallet():
     assert_wallet_amount(wallets["Sender"], SELLER_RECEIVES - RECIPIENT_RECEIVES, SELLER_RECEIVES)
