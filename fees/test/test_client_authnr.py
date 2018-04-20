86597ef0ee02c3e3901df53ed9c5bebf9de1df8d
matt raffel <matt.raffel@evernym.com>
2018-04-20 14:34:18 -0600

    TOK-102  test_verify_signature_incorrect_signatures now works correctly

--- fees/test/test_client_authnr.py
+++ fees/test/test_client_authnr.py
@@ -25,14 +25,10 @@ VALID_IDENTIFIER = "6ouriXMZkLeHsuXrN1X1fd"
 VALID_REQID = 1517423828260117
 CONS_TIME = 1518541344
 PROTOCOL_VERSION = 1
-SIGNATURES = {'B8fV7naUqLATYocqu7yZ8W':
-                  '27BVCWvThxMV9pzqz3JepMLVKww7MmreweYjh15LkwvAH4qwYAMbZWeYr6E6LcQexYAikTHo212U1NKtG8Gr2PPP',
-              'M9BJDuS24bqbJNvBRsoGg3':
-                  '5BzS7J7uSuUePRzLdF5BL5LPvnXxzQyB5BqMT19Hz8QjEyb41Mum71TeNvPW9pKbhnDK12Pciqw9WRHUvsfwdYT5',
-              'E7QRhdcnhAwA6E46k9EtZo':
-                  'MsZsG2uQHFqMvAsQsx5dnQiqBjvxYS1QsVjqHkbvdS2jPdZQhJfackLQbxQ4RDNUrDBy8Na6yZcKbjK2feun7fg',
-              'CA4bVFDU4GLbX8xZju811o':
-                  '3A1Pmkox4SzYRavTj9toJtGBr1Jy9JvTTnHz5gkS5dGnY3PhDcsKpQCBfLhYbKqFvpZKaLPGT48LZKzUVY4u78Ki'}
+SIGNATURES = {'M9BJDuS24bqbJNvBRsoGg3': '5eJax8GW8gTRfZzhuta9s7hU2K3dkKpDWGE7SUsMqiRmQ2GzWXxJKaDzcPMKdZWqrA5Kn1vSHFND9oThsjaQLhHy',
+              'B8fV7naUqLATYocqu7yZ8W': 'AaGqjqGk67mj3MVua46RiqJ6mq6zoy99VriGvZJbpZekhrtju9k2NQrrJcdnMnps7cBZfFxLwhELnLZnTqfb9Ag',
+              'E7QRhdcnhAwA6E46k9EtZo': '2EBZxZ3E2r2ZjCCBwgD6ipnHbskZb4Y4Yqm6haYEsr7hdM1m36yqLFrmNSB7JPqjAsMx6qjw6dWV5sRou1DgiKrM',
+              'CA4bVFDU4GLbX8xZju811o': 'YJjXm8vfiy1sD586tecQ2Eh1Q3wFmLodaxctArasW7RNCujPiZa5CurdW5b8dRXMEBdX9YhsDGkahJXUnZaH8SC'}
 
 
 # ------------------------------------------------------------------------------------
@@ -42,6 +38,7 @@ class FeeData:
         self.protocolVersion = PROTOCOL_VERSION
         self.reqId = VALID_REQID
         self.identifier = VALID_IDENTIFIER
+        self.signatures = SIGNATURES
 
     pass
 
@@ -68,12 +65,12 @@ def node(txnPoolNodeSet):
 def test_authenticate_success():
     state = pruning_state()
     fees_authenticator = FeesAuthNr(state, None)
-    req_data = {'signatures': SIGNATURES,
-                'reqId': VALID_REQID,
-                'identifier': VALID_IDENTIFIER,
-                'operation': {'type': SET_FEES,
-                              'fees': {'1': 4, '10001': 8}
-                              }
+    req_data = {'signatures': {'M9BJDuS24bqbJNvBRsoGg3': '2NTMJMnhhZSghbVViTqy2TWoS272kH3apo8ckD4DQf9YXE4zP7g72jg2DLCEzX5ndxUzHmU74hNPjD3syZ8LuBko',
+                               'B8fV7naUqLATYocqu7yZ8W': '45rSNuKmfGmitsS2jRJaALkGBWDtXSfuuMdLzLkKUL8AS3xk8f4dM6oKDHvMLnGqVsqmd5z5NDvskj5XmwazrBFP',
+                               'CA4bVFDU4GLbX8xZju811o': '4WypVGy9PFKeUio5jAKcGJmsrpnVer9ekm6P2QmN15uRRVeKZux3LzWJ34b4UCdGQEhh7LEv8RbxTXj8Ah8keRBG',
+                               'E7QRhdcnhAwA6E46k9EtZo': '5edfnbZLDrz3QaEeuDp9KVMty9agHJpgeWY1PWHSToLfNqSqYrndsUqfbusPymYKRVojvT9uA65ye3bwioQcbMdi'},
+                'reqId': 1524252730845898,
+                'operation': {'type': '20000', 'fees': {'1': 4, '10001': 8}}
                 }
 
     value = fees_authenticator.authenticate(req_data)
@@ -141,15 +138,15 @@ def test_verify_signature_invalid_signature_format(node):
 
 
 # ------------------------------------------------------------------------------------
-# TODO this one works
-#
+# this one works
+# in this test, the signature in fees is not valid.  it is a valid signature but not valid for this data set
 def test_verify_signature_incorrect_signatures():
     state = pruning_state()
     fees_authenticator = FeesAuthNr(state, None)
     msg = FeeData()
     msg.signatures = {'MSjKTWkPLtYoPEaTF1TUDb': '61PUc8K8aAkhmCjWLstxwRREBAJKbVMRuGiUXxSo1tiRwXgUfVT4TY47NJtbQymcDW3paXPWNqqD4cziJjoPQSSX'}
 
-    setattr(msg, "fees", [[['GMBk8YVHnctVoCmXuxaAundKyfa5KDredBYE5WZaN5V2', 2, 'C4xbW4SRLA8MaBpPfWrHvuoN6VEb8tzc21a2WMYfVGGTjK7KhUBAQaQ36iGqekDxbWuuVdZTu2PE47weiMUrH6E']],
+    setattr(msg, "fees", [[['GMBk8YVHnctVoCmXuxaAundKyfa5KDredBYE5WZaN5V2', 2, '5wuXGeWyrM2xcp68rRsYEaegaguEJBBTDQioeSgDv5jMFeaeSLPAcMs4XwcxNXBwoUAUWgxSMN9WUnZ7ADctdPyQ']],
                            ['GMBk8YVHnctVoCmXuxaAundKyfa5KDredBYE5WZaN5V2', 10]])
 
     with pytest.raises(InsufficientCorrectSignatures):
