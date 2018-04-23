71c3b6b71518f0cae2740f95041559963a84bcb6
matt raffel <matt.raffel@evernym.com>
2018-04-23 11:15:23 -0600

    TOK-102 finishes unit tests for FeesAuthNr.  Cleans up code.

--- fees/test/test_client_authnr.py
+++ fees/test/test_client_authnr.py
@@ -1,31 +1,14 @@
 import pytest
-import json
 
 from plenum.server.plugin.fees.src.client_authnr import FeesAuthNr
 from plenum.common.exceptions import InvalidSignatureFormat, \
     InsufficientCorrectSignatures, InvalidClientRequest
 from plenum.common.constants import DOMAIN_LEDGER_ID
-
-from plenum.server.plugin.fees.test.test_set_get_fees import fees_set
-
-# Constants
-from plenum.server.plugin.fees.src.constants import SET_FEES
-from plenum.server.plugin.token.src.client_authnr import TokenAuthNr
-from state.state import State
 from state.pruning_state import PruningState
 from storage.kv_in_memory import KeyValueStorageInMemory
 
-VALID_ADDR_1 = '6baBEYA94sAphWBA5efEsaA6X2wCdyaH7PXuBtv2H5S1'
-VALID_ADDR_2 = '8kjqqnF3m6agp9auU7k4TWAhuGygFAgPzbNH3shp4HFL'
-VALID_ADDR_3 = '2LWr7i8bsnE3BgX7MNUNEBpiJYjn7uEA1HNyMSJyy59z'
-VALID_ADDR_4 = 'Dy7AxfksXZELTSxF1sUnEocJAJp98E6taMZ9hhLeDtZH'
-VALID_ADDR_5 = 'CDe3vTMgns7nXGFwbGkrz24m2VNghmBScdF3QRstF62g'
-VALID_ADDR_6 = '62tw4B64yEu7HmJyjMgZ2tE5wgyTtsXTpB7n8VAtfmjL'
-VALID_ADDR_7 = 'D2neNhD3mTgN28HKH8qmLBYnaadb9F2GtL5nAzQpkpR6'
-
 VALID_IDENTIFIER = "6ouriXMZkLeHsuXrN1X1fd"
 VALID_REQID = 1517423828260117
-CONS_TIME = 1518541344
 PROTOCOL_VERSION = 1
 SIGNATURES = {'M9BJDuS24bqbJNvBRsoGg3': '5eJax8GW8gTRfZzhuta9s7hU2K3dkKpDWGE7SUsMqiRmQ2GzWXxJKaDzcPMKdZWqrA5Kn1vSHFND9oThsjaQLhHy',
               'B8fV7naUqLATYocqu7yZ8W': 'AaGqjqGk67mj3MVua46RiqJ6mq6zoy99VriGvZJbpZekhrtju9k2NQrrJcdnMnps7cBZfFxLwhELnLZnTqfb9Ag',
@@ -46,14 +29,13 @@ class FeeData:
 
 
 # ------------------------------------------------------------------------------------
-# helper method for creating a state instance which will support the functions
-# of the test
+# creates state instance that will be good enough for some of the tests below
 def pruning_state():
     return PruningState(KeyValueStorageInMemory())
 
 
 # ------------------------------------------------------------------------------------
-# test fixtures
+# gets nodes properly setup, required for some of the tests
 @pytest.fixture
 def node(txnPoolNodeSet):
     a, b, c, d = txnPoolNodeSet
@@ -63,8 +45,8 @@ def node(txnPoolNodeSet):
 
 # ------------------------------------------------------------------------------------
 # authenticate returns a list of authenticated signatures.  it should match the number
-# of inputted signatures since they are all valie
-def test_authenticate_success(fees_set, node):
+# of inputted signatures since they are all value
+def test_authenticate_success(node):
 
     state = node[0].getState(DOMAIN_LEDGER_ID)
     fees_authenticator = FeesAuthNr(state, None)
@@ -89,6 +71,34 @@ def test_authenticate_success(fees_set, node):
     value = fees_authenticator.authenticate(req_data)
     assert value is not None
     assert 4 == len(value)
+
+
+# ------------------------------------------------------------------------------------
+# only 2 of the 4 signatures are valid.  (hint: the last two are mucky)
+def test_authenticate_errors_on_invalid_inputs(node):
+
+    state = node[0].getState(DOMAIN_LEDGER_ID)
+    fees_authenticator = FeesAuthNr(state, None)
+
+    req_data = {
+        'signatures':
+            {
+                'E7QRhdcnhAwA6E46k9EtZo': '32H37GfuchojdbNeMxwNUhZJwWyJCz48aP5u1AvN3xhNramVqrq74H4pE8LKMgZw7rAdyrPvHUWzWAZdB243fqhA',
+                'CA4bVFDU4GLbX8xZju811o': '3tkZU65KeybmkUcrA6HuovVTDD8vsVm2VvB7bpUhPt2MLpez6eRrRvysUutusJz6xryCtk7g1b115pKhNGcqRTss',
+                'M9BJDuS24bqbJNvBRsoGg3': '00XHdWTeWYRpoyCAZgpM7qA7Ms2MRYu6NpasPagPQRGoUukE2NdSXGonu6dWgsPNgybqW7fotw9BjccXAy7BzMsY',
+                'B8fV7naUqLATYocqu7yZ8W': '00q55hTSBafovXS9gTNkW1GM9vVF6Y4s2fLEsmew9DaN95rmZ5ZwXj74NgTmGeGszPomWXPsRr5QGNZb6GsG57PV'
+            },
+        'reqId': 1524500821797147,
+        'operation':
+            {
+                'fees': {'10001': 8, '1': 4},
+                'type': '20000'
+            },
+        'protocolVersion': 1
+    }
+
+    with pytest.raises(InvalidSignatureFormat):
+        fees_authenticator.authenticate(req_data)
 
 
 # ------------------------------------------------------------------------------------
