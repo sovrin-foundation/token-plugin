5a02728f981f9ce5aea278251044323ff65e9ef1
Andrey Kononykhin <andkononykhin@gmail.com>
2018-06-25 12:53:57 +0300

    improves package metadata routine
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- sovtoken/__metadata__.py
+++ sovtoken/__metadata__.py
@@ -1,9 +1,28 @@
 """
-plenum package metadata
+sovtoken package metadata
 """
+
+# TODO: Update the field values below where needed
+__title__ = 'sovtoken'
 __version_info__ = (1, 2)
 __version__ = '.'.join(map(str, __version_info__))
+__description__ = 'Token Plugin For Indy Plenum'
+__long_description__ = ''
+__url__ = 'https://github.com/evernym/plugin/tree/master/sovtoken'
 __author__ = "Evernym"
+__author_email__ = ''
+__maintainer__ = "Evernym"
 __license__ = "Apache 2.0"
 
-__all__ = ['__version_info__', '__version__', '__author__', '__license__']
+__all__ = [
+    '__title__',
+    '__version_info__',
+    '__version__',
+    '__description__',
+    '__long_description__',
+    '__url__',
+    '__author__',
+    '__author_email__',
+    '__maintainer__',
+    '__license__'
+]
