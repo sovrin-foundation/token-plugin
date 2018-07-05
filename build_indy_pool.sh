de0ec633509127b6f78b406d62dccc928e9b6c78
cam-parra <caeparra@gmail.com>
2018-07-05 16:16:03 -0600

    debian packages will be added upon request
    Signed-off-by: cam-parra <caeparra@gmail.com>

--- build_indy_pool.sh
+++ build_indy_pool.sh
@@ -1,11 +1,19 @@
 export MAKE=/usr/bin/make
-eval "SRC_DIR_NAME=sovtoken make -C devops/ package_in_docker"
-eval "SRC_DIR_NAME=sovtokenfees make -C devops/ package_in_docker"
 
+red=`tput setaf 1`
+green=`tput setaf 2`
+echo "${red}BEGIN BUILDING: ${green}sovtoken.deb "
+eval "SRC_DIR_NAME=sovtoken make -C devops/ package_in_docker" > /dev/null
+echo "${red}BEGIN BUILDING: ${green}sovtokenfees.deb "
+eval "SRC_DIR_NAME=sovtokenfees make -C devops/ package_in_docker" > /dev/null
+echo "${red}MOVING: ${green}sovtoken.deb "
 mv devops/_build/sovtoken/*.deb devops/build-scripts/xenial/Pool_Party/.
+echo "${red}MOVING: ${green}sovtoken.deb "
 mv devops/_build/sovtokenfees/*.deb devops/build-scripts/xenial/Pool_Party/.
 
 cd devops/build-scripts/xenial/Pool_Party/
-docker build -t indy_pool .
-
-docker run -itd -p 9701-9708:9701-9708 indy_pool
+echo "${red}BUILD: ${green} payment ledger"
+docker build -t indy_pool . > /dev/null
+echo "${red}RUNNING: ${green} payment ledger"
+docker run -itd -p 9701-9708:9701-9708 indy_pool > /dev/null
+echo "${red}Payment Ledger Success: ${green} payment ledger"
