5c8b9863cac00f5764e0d4a5250db8f8919da867
cam-parra <caeparra@gmail.com>
2018-07-20 05:51:09 -0600

    setup.py matches master
    Signed-off-by: cam-parra <caeparra@gmail.com>

--- sovtoken/setup.py
+++ sovtoken/setup.py
@@ -38,7 +38,7 @@ setup(
         '': ['*.txt', '*.md', '*.rst', '*.json', '*.conf', '*.html',
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', 'sovtoken']},
     include_package_data=True,
-    install_requires=['indy-plenum-dev==1.4.442'],
+    install_requires=['indy-plenum==1.4.442'],
     setup_requires=['pytest-runner'],
     extras_require={
         'test': tests_require,
