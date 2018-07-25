69b1eaf67ddf381d6d5b1da63b4cf2741a609133
dastardlychimp <darien.hess@evernym.com>
2018-07-25 07:52:32 -0600

    Fix test_public_xfer_2 (#61)
    * Fixed test_seller_xfer_double_spend_attempt
    
    Organized test, formed helper functions, added some comments.
    Test is now much cleaner.
    
    Also, now expecting failure on test_get_multiple_addresses
    
    * Fix test_xfer_with_multiple_inputs
    
    Breaking to previous test. Now it mints its own
    tokens and creates its own addresses.
    
    Refactored so it is much easier to read.

--- sovtoken/sovtoken/test/test_public_xfer_2.py
+++ sovtoken/sovtoken/test/test_public_xfer_2.py
@@ -8,11 +8,11 @@ from plenum.common.util import lxor
 from sovtoken.constants import OUTPUTS
 from sovtoken.util import update_token_wallet_with_result
 from sovtoken.wallet import TokenWallet, Address
-from sovtoken.test.helper import send_xfer, \
+from sovtoken.test.helper import send_xfer, send_public_mint, \
     check_output_val_on_all_nodes, xfer_request, send_get_utxo
 from sovtoken.test.conftest import seller_gets, total_mint
 from plenum.test.helper import sdk_send_signed_requests, \
-    sdk_get_and_check_replies
+    sdk_get_replies, sdk_check_reply
 
 
 @pytest.fixture(scope="module")
@@ -150,60 +150,95 @@ def test_seller_xfer_valid(valid_xfer_txn_done):
     """
     pass
 
-@pytest.mark.skip
+
 def test_seller_xfer_double_spend_attempt(looper, sdk_pool_handle,  # noqa
-                                          nodeSetWithIntegratedTokenPlugin,
-                                          seller_token_wallet, seller_address,
-                                          valid_xfer_txn_done, user1_address,
-                                          user2_address):
+    nodeSetWithIntegratedTokenPlugin, public_minting, sdk_wallet_client,
+    seller_address, seller_token_wallet, user1_address, user2_address):
     """
     An address tries to send to send to transactions using the same UTXO,
     one of the txn gets rejected even though the amount held by the UTXO is
     greater than the sum of outputs in both txns, since one UTXO can be used
     only once
     """
-    seq_no = get_seq_no(valid_xfer_txn_done)
+
+    # =============
+    # Declaration of helper functions.
+    # =============
+
+    def succeeded(req_resp):
+        try:
+            sdk_check_reply(req_resp)
+            return True
+        except Exception:
+            return False
+
+    def check_output_val(address, amount):
+        return check_output_val_on_all_nodes(
+            nodeSetWithIntegratedTokenPlugin,
+            address,
+            amount
+        )
+
+    def check_no_output_val(address, amount):
+        with pytest.raises(AssertionError):
+            check_output_val(address, amount)
+
+    def get_seq_no_first_utxo(address):
+        get_utxo_resp = send_get_utxo(
+            looper,
+            address,
+            sdk_wallet_client,
+            sdk_pool_handle
+        )
+
+        return get_utxo_resp[OUTPUTS][0][1]
+
+    # =============
+    # Form the two xfer requests. Each request will try to spend the same UTXO.
+    # =============
+
     user1_gets = 3
     user2_gets = 5
+    seq_no = get_seq_no_first_utxo(seller_address)
     inputs = [[seller_token_wallet, seller_address, seq_no]]
-    seller_remaining = seller_gets - user1_gets
-    outputs1 = [[user1_address, user1_gets], [seller_address, seller_remaining]]
-    seller_remaining -= user2_gets
-    outputs2 = [[user2_address, user2_gets], [seller_address, seller_remaining]]
+    outputs1 = [
+        [user1_address, user1_gets],
+        [seller_address, seller_gets - user1_gets]
+    ]
+    outputs2 = [
+        [user2_address, user2_gets],
+        [seller_address, seller_gets - user2_gets]
+    ]
     r1 = xfer_request(inputs, outputs1)
-    r1 = sdk_send_signed_requests(sdk_pool_handle, [json.dumps(r1.as_dict), ])
     r2 = xfer_request(inputs, outputs2)
-    r2 = sdk_send_signed_requests(sdk_pool_handle, [json.dumps(r2.as_dict), ])
-    # So that both requests are sent simultaneously
-    looper.runFor(.2)
+    requests = [json.dumps(r.as_dict) for r in [r1, r2]]
 
-    # Both requests should not be successful, one and only one should be
-    success1, success2 = False, False
-    try:
-        check_output_val_on_all_nodes(nodeSetWithIntegratedTokenPlugin,
-                                      user1_address, user1_gets)
-        success1 = True
-    except Exception:
-        pass
+    # =============
+    # Send the two requests and wait for replies. Only one request
+    # should succeed.
+    # =============
 
-    try:
-        check_output_val_on_all_nodes(nodeSetWithIntegratedTokenPlugin, user2_address, user2_gets)
-        success2 = True
-    except Exception:
-        pass
+    req_resp = sdk_send_signed_requests(sdk_pool_handle, requests)
+    req_resp = sdk_get_replies(looper, req_resp)
+
+    success1 = succeeded(req_resp[0])
+    success2 = succeeded(req_resp[1])
 
     assert lxor(success1, success2)
 
-    request = r1 if success1 else r2
-    _, rep = sdk_get_and_check_replies(looper, request)[0]
-    update_token_wallet_with_result(seller_token_wallet, rep['result'])
+    # =============
+    # Check that the seller, user1, and user2, have the output or not.
+    # =============
 
     if success1:
-        check_output_val_on_all_nodes(nodeSetWithIntegratedTokenPlugin, seller_address,
-                                      seller_gets - user1_gets)
+        check_output_val(seller_address, seller_gets - user1_gets)
+        check_output_val(user1_address, user1_gets)
+        check_no_output_val(user2_address, 0)
     else:
-        check_output_val_on_all_nodes(nodeSetWithIntegratedTokenPlugin, seller_address,
-                                      seller_gets - user2_gets)
+        check_output_val(seller_address, seller_gets - user2_gets)
+        check_output_val(user2_address, user2_gets)
+        check_no_output_val(user1_address, 0)
+
 
 
 def test_query_utxo(looper, sdk_pool_handle, sdk_wallet_client, seller_token_wallet,  # noqa
@@ -229,56 +264,101 @@ def test_query_utxo(looper, sdk_pool_handle, sdk_wallet_client, seller_token_wal
     res3 = send_get_utxo(looper, address.address, sdk_wallet_client, sdk_pool_handle)
     assert len(res3[OUTPUTS]) == 0
 
-@pytest.mark.skip
+
+# We can't handle multiple addresses at the moment because it requires a more 
+# complicated state proof. So this test has been changed to show that multiple
+# addresses are not accepted.
 def test_get_multiple_addresses(public_minting, looper, sdk_wallet_client, sdk_pool_handle, seller_address, SF_address):
     non_existent_address = Address().address
     addresses_to_check = [seller_address, SF_address, non_existent_address]
-    resp = send_get_utxo(looper, addresses_to_check, sdk_wallet_client, sdk_pool_handle)
-    address_in_outputs = lambda a: any(filter(lambda utxo: utxo[0] == a, resp[OUTPUTS]))
-    assert address_in_outputs(seller_address)
-    assert address_in_outputs(SF_address)
-    assert not address_in_outputs(non_existent_address)
+    with pytest.raises(RequestNackedException):
+        resp = send_get_utxo(looper, addresses_to_check, sdk_wallet_client, sdk_pool_handle)
+        # def address_in_outputs(address):
+        #     return any(filter(lambda utxo: utxo[0] == a, resp[OUTPUTS]))
+
+        # assert address_in_outputs(seller_address)
+        # assert address_in_outputs(SF_address)
+        # assert not address_in_outputs(non_existent_address)
 
 
-@pytest.mark.skip
-def test_xfer_with_multiple_inputs(public_minting, looper,  # noqa
-                                   sdk_pool_handle, sdk_wallet_client,
-                                   seller_token_wallet, seller_address,
-                                   user1_address):
+def test_xfer_with_multiple_inputs(looper,  # noqa
+                                   sdk_pool_handle,
+                                   sdk_wallet_client,
+                                   seller_token_wallet,
+                                   trustee_wallets):
     """
     3 inputs are used to transfer tokens to a single output
     """
-    res = send_get_utxo(looper, seller_address, sdk_wallet_client, sdk_pool_handle)
-    update_token_wallet_with_result(seller_token_wallet, res)
-    utxos = [_ for lst in seller_token_wallet.get_all_wallet_utxos().values()
-             for _ in lst]
-    assert utxos
-    old_count = len(utxos)
+
+    # =============
+    # Declaration of helper functions.
+    # =============
+
+    def create_addresses_wallet(wallet, num):
+        addresses = []
+        for _ in range(num):
+            address = Address()
+            wallet.add_new_address(address=address)
+            addresses.append(address.address)
+
+        return addresses
+    
+    def update_wallet_utxos(wallet, address):
+        res = send_get_utxo(looper, address, sdk_wallet_client, sdk_pool_handle)
+        update_token_wallet_with_result(seller_token_wallet, res)
+
+    def xfer_tokens(wallet, inputs, outputs):
+        inputs = [
+            [wallet, address, seq_no]
+            for [address, seq_no] in inputs
+        ]
+        res = send_xfer(looper, inputs, outputs, sdk_pool_handle)
+        update_token_wallet_with_result(wallet, res)
+
+    def addresses_utxos(wallet, address):
+        return wallet.addresses[address].all_utxos
+
+    def mint_tokens(outputs):
+        send_public_mint(looper, trustee_wallets, outputs, sdk_pool_handle)
+
+    # =============
+    # Mint tokens to sender's address
+    # =============
+
+    first_address = create_addresses_wallet(seller_token_wallet, 1)[0]
+    outputs = [[first_address, 50]]
+    mint_tokens(outputs)
+    update_wallet_utxos(seller_token_wallet, first_address)
+    (seq_no, amount) = addresses_utxos(seller_token_wallet, first_address)[0]
+
+    # =============
+    # Transfer tokens to 3 different addresses
+    # =============
+
     # Add 3 new addresses
-    new_addrs = []
-    for i in range(3):
-        new_addrs.append(Address())
-        seller_token_wallet.add_new_address(address=new_addrs[-1])
-    seq_no, amount = utxos[0]
+    new_addresses = create_addresses_wallet(seller_token_wallet, 3)
+
     # Distribute an existing UTXO among 3 address
-    inputs = [[seller_token_wallet, seller_address, seq_no]]
-    outputs = [[addr.address, amount // 3] for addr in new_addrs]
+    inputs = [[first_address, seq_no]]
+    outputs = [[address, amount // 3] for address in new_addresses]
     outputs[-1][1] += amount % 3
-    res = send_xfer(looper, inputs, outputs, sdk_pool_handle)
-    update_token_wallet_with_result(seller_token_wallet, res)
+    xfer_tokens(seller_token_wallet, inputs, outputs)
 
-    new_utxos = [_ for lst in seller_token_wallet.get_all_wallet_utxos().values()
-                 for _ in lst]
-    # Since 1 existing UTXO is spent and 3 new created
-    assert len(new_utxos) - old_count == 2
+    # =============
+    # Assert tokens are in new addresses
+    # =============
 
-    new_seq_no = new_utxos[0][0]
-    sum_utxo_val = sum(t[1] for t in new_utxos)
-    seller_gets = sum_utxo_val - sum_utxo_val
-    inputs = [[seller_token_wallet, addr.address, new_seq_no]
-              for addr in new_addrs]
-    outputs = [[user1_address, sum_utxo_val], ]
-    res = send_xfer(looper, inputs, outputs, sdk_pool_handle)
-    update_token_wallet_with_result(seller_token_wallet, res)
-    assert seller_token_wallet.get_total_wallet_amount() == seller_gets
+    assert 16 == addresses_utxos(seller_token_wallet, new_addresses[0])[0][1]
+    assert 16 == addresses_utxos(seller_token_wallet, new_addresses[1])[0][1]
+    assert 18 == addresses_utxos(seller_token_wallet, new_addresses[2])[0][1]
+
+    # =============
+    # Transfer tokens from 3 addresses back to a single address
+    # =============
+
+    inputs = [[address, seq_no + 1] for address in new_addresses]
+    outputs = [[first_address, amount]]
+    xfer_tokens(seller_token_wallet, inputs, outputs)
+
+    assert seller_token_wallet.get_total_address_amount(first_address) == amount
 
