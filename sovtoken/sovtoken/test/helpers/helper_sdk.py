259c61b66bed1ebd068b45c32c793aa866798531
dastardlychimp <darien.hess@evernym.com>
2018-07-27 14:08:16 -0600

    Add `get_first_result` method to HelperSdk

--- sovtoken/sovtoken/test/helpers/helper_sdk.py
+++ sovtoken/sovtoken/test/helpers/helper_sdk.py
@@ -1,18 +1,24 @@
 import json
 import plenum.test.helper as plenum_helper
 
+from sovtoken.constants import RESULT
+
 
 class HelperSdk():
     """
     HelperSdk is a class which abstracts away the looper, pool_handle, and
     node_pool in the sdk calls.
 
-    # methods for sending request objects
+    # Methods
+
+    - get_first_result
+
+    ## methods for sending request objects
     - prepare_request_objects
     - send_and_check_request_objects
     - send_request_objects
 
-    # ported sdk methods
+    ## ported sdk methods
     - sdk_get_and_check_replies
     - sdk_send_and_check
     - sdk_send_signed_requests
@@ -22,6 +28,10 @@ class HelperSdk():
         self._looper = looper
         self._pool_handle = pool_handle
         self._node_pool = node_pool
+
+    def get_first_result(self, request_response_list):
+        """ Gets the result field from the first response """
+        return request_response_list[0][1][RESULT]
 
     # =============
     # Request object methods
