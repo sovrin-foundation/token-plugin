cd6300d2d11116b0b7a1bf84d51b87f598bc8450
dastardlychimp <darien.hess@evernym.com>
2018-07-27 14:07:14 -0600

    Add `get_utxo` method to HelperRequest
    Signed-off-by: dastardlychimp <darien.hess@evernym.com>

--- sovtoken/sovtoken/test/helpers/helper_request.py
+++ sovtoken/sovtoken/test/helpers/helper_request.py
@@ -1,7 +1,7 @@
 from plenum.common.constants import TXN_TYPE, CURRENT_PROTOCOL_VERSION
 from plenum.common.request import Request
 from sovtoken.constants import INPUTS, OUTPUTS, EXTRA, SIGS, XFER_PUBLIC, \
-    MINT_PUBLIC
+    MINT_PUBLIC, GET_UTXO, ADDRESS
 from sovtoken.util import address_to_verkey
 
 
@@ -10,12 +10,25 @@ class HelperRequest():
     Helper to build different requests.
 
     # methods
+    - get_utxo
     - transfer
     - mint
     """
 
-    def __init__(self, helper_wallet):
-        self.wallet = helper_wallet
+    def __init__(self, helper_wallet, client_did):
+        self._wallet = helper_wallet
+        self._client_did = client_did
+
+    def get_utxo(self, address):
+        """ Builds a get_utxo request """
+        payload = {
+            TXN_TYPE: GET_UTXO,
+            ADDRESS: address.address
+        }
+
+        request = self._create_request(payload, self._client_did)
+
+        return request
 
     def transfer(self, inputs, outputs, extra=None):
         """ Builds a transfer request """
@@ -23,7 +36,7 @@ class HelperRequest():
         inputs_ready = [[address.address, seq_no] for address, seq_no in inputs]
 
         [first_address, seq_no] = inputs_ready[0]
-        payment_signatures = self.wallet.payment_signatures(inputs, outputs_ready)
+        payment_signatures = self._wallet.payment_signatures(inputs, outputs)
 
         payload = {
             TXN_TYPE: XFER_PUBLIC,
@@ -47,7 +60,7 @@ class HelperRequest():
         }
 
         request = self._create_request(payload)
-        request = self.wallet.sign_request_trustees(request)
+        request = self._wallet.sign_request_trustees(request)
         return request
 
     def _prepare_outputs(self, outputs):
