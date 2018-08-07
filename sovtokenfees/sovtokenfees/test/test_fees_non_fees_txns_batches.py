7df04cd8752597c862b9071fefa351b2206543f5
matt raffel <matt.raffel@evernym.com>
2018-08-07 14:06:04 -0600

    TOK-348 Added one additional test
    Signed-off-by: matt raffel <matt.raffel@evernym.com>

--- sovtokenfees/sovtokenfees/test/test_fees_non_fees_txns_batches.py
+++
@@ -1,4 +0,0 @@
-# TODO: Test batch with sovtokenfees and non-sovtokenfees txns,
-# TODO: Test when a batch of non sovtokenfees txns followed by sovtokenfees txns followed
-# by non sovtokenfees txns
-
