b23e124f19342e01afbd0533f1e0abbad5d37f58
dastardlychimp <darien.hess@evernym.com>
2018-08-15 14:08:46 -0600

    TOK-340 Add future tests for minting.
    -Created some future tests for quorum minting. They are currently skipped.
    - Added a HelperNode to sovtoken.
    - Updated HelperRequest.nym to be able to
    update nyms, not only create them.
    
    Signed-off-by: dastardlychimp <darien.hess@evernym.com>

--- sovtoken/sovtoken/test/helpers/helper_wallet.py
+++ sovtoken/sovtoken/test/helpers/helper_wallet.py
@@ -1,3 +1,8 @@
+import json
+
+from indy import did
+from plenum.client.wallet import Wallet
+from plenum.common.util import randomString
 from sovtoken.test.wallet import Address
 
 
@@ -7,13 +12,18 @@ class HelperWallet():
 
     # Methods
     - add_new_addresses
+    - create_address
+    - create_client_wallet
+    - create_new_addresses
     - payment_signatures
     - sign_request
     - sign_request_trustees
     - sign_request_stewards
     """
 
-    def __init__(self, trustee_wallets, steward_wallets):
+    def __init__(self, looper, client_wallet, trustee_wallets, steward_wallets):
+        self._looper = looper
+        self._client_wallet = client_wallet
         self._trustee_wallets = trustee_wallets
         self._steward_wallets = steward_wallets
 
@@ -31,6 +41,25 @@ class HelperWallet():
             wallet.add_new_address(address=address)
 
         return addresses
+
+    def create_client_wallet(self, seed):
+        """ Create a plenum client wallet from a seed. """
+        wallet = Wallet()
+        wallet.addIdentifier(seed=seed.encode())
+        return wallet
+
+    def create_did(self, seed=None, sdk_wallet=None):
+        """ Create and store a did in a sdk_wallet. """
+        if not seed:
+            seed = randomString(32)
+
+        sdk_wallet = sdk_wallet or self._client_wallet
+
+        wallet_handle, _ = sdk_wallet
+        config = json.dumps({seed: seed})
+        future = did.create_and_store_my_did(wallet_handle, config)
+
+        return self._looper.loop.run_until_complete(future)
 
     def payment_signatures(self, inputs, outputs):
         """ Generate a list of payment signatures from inptus and outputs. """
