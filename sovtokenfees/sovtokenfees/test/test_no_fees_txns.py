1839be55496118fc25578c728ed6d80efc79bb98
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2018-08-29 18:28:12 +0300

    Remove invalid test
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- sovtokenfees/sovtokenfees/test/test_no_fees_txns.py
+++ sovtokenfees/sovtokenfees/test/test_no_fees_txns.py
@@ -16,25 +16,3 @@ def test_txn_with_no_fees_specified(helpers):
     helpers.general.do_set_fees(TXN_FEES)
     request = helpers.request.nym()
     helpers.sdk.send_and_check_request_objects([request])
-
-
-def test_txn_with_0_fees_specified(helpers):
-    helpers.general.do_set_fees(TXN_FEES)
-    address = helpers.wallet.create_address()
-    helpers.general.do_mint([[address, 1000]])
-    utxos = helpers.general.get_utxo_addresses([address])[0]
-
-    request = helpers.request.nym()
-    helpers.request.add_fees(
-        request,
-        utxos,
-        fee_amount=0,
-        change_address=address
-    )
-    responses = helpers.sdk.send_and_check_request_objects([request])
-    result = helpers.sdk.get_first_result(responses)
-    fee_seq_no = get_seq_no(result[FEES])
-
-    utxos = helpers.general.get_utxo_addresses([address])[0]
-
-    assert utxos == [[address, fee_seq_no, 1000]]
