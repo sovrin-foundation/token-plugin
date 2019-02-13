d3d12ae7a603e70293833d06cd283f310dd3f6f3
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2019-02-13 20:08:18 +0300

    [INDY-1874] move indy-plenum to indy-plenum-dev
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- sovtoken/setup.py
+++ sovtoken/setup.py
@@ -39,7 +39,7 @@ setup(
     include_package_data=True,
 
 
-    install_requires=['indy-plenum>=1.6.679'],
+    install_requires=['indy-plenum-dev>=1.6.679'],
     setup_requires=['pytest-runner'],
     extras_require={
         'test': tests_require,
