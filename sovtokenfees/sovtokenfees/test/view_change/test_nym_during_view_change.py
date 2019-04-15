9469c15ba2333562c1ab873c72caea113ced56e8
Andrey Kononykhin <andkononykhin@gmail.com>
2019-04-15 19:21:55 +0300

    fixes test name
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- sovtokenfees/sovtokenfees/test/view_change/test_nym_during_view_change.py
+++ sovtokenfees/sovtokenfees/test/view_change/test_nym_during_view_change.py
@@ -5,9 +5,10 @@ from plenum.common.exceptions import RequestRejectedException
 from sovtokenfees.test.view_change.helper import scenario_txns_during_view_change
 
 
-def test_xfer_during_view_change(
+def test_nym_during_view_change(
         looper,
         nodeSetWithIntegratedTokenPlugin,
+
         fees_set,
         curr_utxo,
         send_and_check_nym_with_fees_curr_utxo
