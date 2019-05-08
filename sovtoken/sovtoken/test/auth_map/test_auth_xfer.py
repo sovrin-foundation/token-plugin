5ec03aa80a9d8355ffdd18d000f069a780a48c6f
Nikita Khateev <hateevnikita@yandex.ru>
2019-05-08 19:19:43 +0300

    Fix one more addres issue
    Signed-off-by: Nikita Khateev <hateevnikita@yandex.ru>

--- sovtoken/sovtoken/test/auth_map/test_auth_xfer.py
+++ sovtoken/sovtoken/test/auth_map/test_auth_xfer.py
@@ -14,6 +14,11 @@ from plenum.test.helper import sdk_sign_request_objects, sdk_send_and_check
 
 
 @pytest.fixture()
+def addresses(helpers):
+    return helpers.wallet.create_new_addresses(2)
+
+
+@pytest.fixture()
 def mint_tokens(helpers, addresses):
     outputs = [{ADDRESS: addresses[0], AMOUNT: 1000}]
     return helpers.general.do_mint(outputs)
