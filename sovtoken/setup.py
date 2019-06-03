7b187d60b5cd641db8d82564fc1b9552926e0fa9
toktar <renata.toktar@dsr-corporation.com>
2019-06-03 15:33:06 +0300

    Bump node and plugins version
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- sovtoken/setup.py
+++ sovtoken/setup.py
@@ -38,7 +38,7 @@ setup(
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', 'sovtoken']},
     include_package_data=True,
 
-    install_requires=['indy-node==1.8.0.dev981'],
+    install_requires=['indy-node==1.9.0.dev985'],
 
     setup_requires=['pytest-runner'],
     extras_require={
