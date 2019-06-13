b30d801bfdbbc8de8407340c6596bac5d44502c0
toktar <renata.toktar@dsr-corporation.com>
2019-06-13 18:56:05 +0300

    INDY-2077: fix _validate_metadata and add test_forbid_set_fees
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- sovtokenfees/sovtokenfees/test/auth_map/test_auth_set_fees.py
+++ sovtokenfees/sovtokenfees/test/auth_map/test_auth_set_fees.py
@@ -3,7 +3,7 @@ import json
 import pytest
 from indy_node.test.auth_rule.helper import sdk_send_and_check_auth_rule_request
 from indy_common.authorize.auth_actions import ADD_PREFIX, EDIT_PREFIX
-from indy_common.authorize.auth_constraints import AuthConstraint
+from indy_common.authorize.auth_constraints import AuthConstraint, AuthConstraintForbidden
 from sovtokenfees.constants import SET_FEES, FEES
 from sovtokenfees.sovtokenfees_auth_map import sovtokenfees_auth_map, edit_fees
 
@@ -72,6 +72,46 @@ def test_set_fees(helpers,
     with pytest.raises(RequestRejectedException, match="Not enough STEWARD signatures"):
         set_fees(helpers, fees)
     set_fees(helpers, fees, trustee=False)
+
+    sdk_send_and_check_auth_rule_request(looper,
+                                         sdk_pool_handle,
+                                         sdk_wallet_trustee,
+                                         auth_action=EDIT_PREFIX,
+                                         auth_type=SET_FEES,
+                                         field='*',
+                                         old_value='*',
+                                         new_value='*',
+                                         constraint=sovtokenfees_auth_map[edit_fees.get_action_id()].as_dict)
+    set_fees(helpers, fees)
+
+
+def test_forbid_set_fees(helpers,
+                         fees,
+                         addresses,
+                         looper,
+                         sdk_wallet_trustee,
+                         sdk_pool_handle):
+    """
+    1. Send a SET_FEES txn.
+    2. Forbid SET_FEES via the auth rule for editing SET_FEES.
+    3. Send a SET_FEES txn and check that action forbidden.
+    4. Change the auth rule to a default value.
+    6. Send and check a SET_FEES txn.
+    """
+    set_fees(helpers, fees)
+
+    sdk_send_and_check_auth_rule_request(looper,
+                                         sdk_pool_handle,
+                                         sdk_wallet_trustee,
+                                         auth_action=EDIT_PREFIX,
+                                         auth_type=SET_FEES,
+                                         field='*',
+                                         old_value='*',
+                                         new_value='*',
+                                         constraint=AuthConstraintForbidden().as_dict)
+
+    with pytest.raises(RequestRejectedException, match=str(AuthConstraintForbidden())):
+        set_fees(helpers, fees)
 
     sdk_send_and_check_auth_rule_request(looper,
                                          sdk_pool_handle,
