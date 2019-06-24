c6e8d9124423eca03d3ee49480c684e2eb34c590
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2019-06-24 16:24:46 +0300

    ST-601 Remove key from state on UTXO spending.
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- sovtokenfees/sovtokenfees/test/req_handlers/domain_fee_handler/test_domain_fee_handler_update_state.py
+++ sovtokenfees/sovtokenfees/test/req_handlers/domain_fee_handler/test_domain_fee_handler_update_state.py
@@ -8,6 +8,7 @@ def test_domain_fee_handler_update_state(domain_fee_handler, nym_request_with_fe
     utxo_cache = domain_fee_handler.utxo_cache
 
     assert int(token_state.get((payment_address[8:] + ":2").encode(), isCommitted=False)) == 9
+    assert not token_state.get((payment_address[8:] + ":1").encode(), isCommitted=False)
     assert utxo_cache.get(payment_address[8:]) == '2:9'
     assert domain_fee_handler._fees_tracker.fees_in_current_batch == 1
     assert domain_fee_handler._fees_tracker.has_deducted_fees(NYM, 1)
