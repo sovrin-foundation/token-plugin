b52f17282676dfabf1aa319db3b755152a403af5
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2019-06-26 12:31:06 +0300

    fix batch handler tests
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- sovtokenfees/sovtokenfees/test/req_handlers/fees_batch_handler/conftest.py
+++ sovtokenfees/sovtokenfees/test/req_handlers/fees_batch_handler/conftest.py
@@ -1,7 +1,18 @@
 import pytest
+from sovtoken import TOKEN_LEDGER_ID
 from sovtokenfees.req_handlers.batch_handlers.fee_batch_handler import DomainFeeBatchHandler
+
+from plenum.common.ledger_uncommitted_tracker import LedgerUncommittedTracker
 
 
 @pytest.fixture
-def fee_batch_handler(db_manager_with_config, fees_tracker):
+def fee_batch_handler(db_manager_with_config, fees_tracker, token_tracker):
     return DomainFeeBatchHandler(db_manager_with_config, fees_tracker)
+
+
+@pytest.fixture
+def token_tracker(db_manager_with_config):
+    token_tracker = LedgerUncommittedTracker(db_manager_with_config.get_state(TOKEN_LEDGER_ID).committedHeadHash,
+                                             db_manager_with_config.get_ledger(TOKEN_LEDGER_ID).committed_root_hash,
+                                             db_manager_with_config.get_ledger(TOKEN_LEDGER_ID).size)
+    db_manager_with_config.register_new_tracker(TOKEN_LEDGER_ID, token_tracker)
