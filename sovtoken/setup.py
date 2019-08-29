54f69e167c8fddb671d4c17fae10245243258cea
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2019-08-29 14:41:33 +0300

    [RC-1.0.2] bump indy-node to rc version
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- sovtoken/setup.py
+++ sovtoken/setup.py
@@ -38,7 +38,7 @@ setup(
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', 'sovtoken']},
     include_package_data=True,
 
-    install_requires=['indy-node==1.9.2'],
+    install_requires=['indy-node==1.9.2rc1'],
 
     setup_requires=['pytest-runner'],
     extras_require={
