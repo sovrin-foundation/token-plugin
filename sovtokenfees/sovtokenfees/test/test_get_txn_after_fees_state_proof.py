56b4bb33a658f3671977017b99879f18489d9de4
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2019-09-10 17:06:02 +0300

    ST-623 -- Fix Yield
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- sovtokenfees/sovtokenfees/test/test_get_txn_after_fees_state_proof.py
+++ sovtokenfees/sovtokenfees/test/test_get_txn_after_fees_state_proof.py
@@ -4,6 +4,7 @@ from plenum.common.txn_util import get_seq_no
 from plenum.test.delayers import req_delay
 from plenum.test.stasher import delay_rules
 from sovtoken import TOKEN_LEDGER_ID
+from sovtoken.constants import AMOUNT, ADDRESS
 from sovtokenfees.constants import FEES
 from sovtokenfees.test.helper import pay_fees
 
@@ -18,16 +19,24 @@ def make_tokens(helpers, nodeSetWithIntegratedTokenPlugin, addresses):
     address, address_2 = addresses
     total = 1000
     outputs = [{"address": address_2, "amount": total}]
-    helpers.general.do_mint(outputs)
+    return helpers.general.do_mint(outputs)
 
 
-@pytest.fixture(scope='function')
-def fees_paid(helpers, nodeSetWithIntegratedTokenPlugin, addresses, fees_set, make_tokens):
-    return pay_fees(helpers, fees_set, addresses[1])
+@pytest.fixture(scope='function', params=['XFER', 'FEES', 'MINT'])
+def last_txn(request, helpers, nodeSetWithIntegratedTokenPlugin, addresses, fees_set, make_tokens):
+    if request.param == 'FEES':
+        return pay_fees(helpers, fees_set, addresses[1])[FEES]
+    if request.param == 'XFER':
+        inputs = helpers.general.get_utxo_addresses(addresses[1:])
+        inputs = [utxo for utxos in inputs for utxo in utxos]
+        outputs = [{ADDRESS: addresses[0], AMOUNT: 992}]
+        return helpers.general.do_transfer(inputs, outputs)
+    if request.param == 'MINT':
+        return make_tokens
 
 
 @pytest.fixture(scope='function', params=['all_responding', 'one_responding'])
-def nodeSetWithOneRespondingNode(request, nodeSetWithIntegratedTokenPlugin, make_tokens, fees_paid):
+def nodeSetWithOneRespondingNode(request, nodeSetWithIntegratedTokenPlugin, make_tokens, last_txn):
     if request.param == 'all_responding':
         yield nodeSetWithIntegratedTokenPlugin
     else:
@@ -36,6 +45,6 @@ def nodeSetWithOneRespondingNode(request, nodeSetWithIntegratedTokenPlugin, make
             yield nodeSetWithIntegratedTokenPlugin
 
 
-def test_get_txn_audit_proof_after_txn_with_fees(helpers, addresses, fees_paid, nodeSetWithOneRespondingNode):
-    seq_no = get_seq_no(fees_paid[FEES])
+def test_get_txn_audit_proof_after_txn_with_fees(helpers, addresses, last_txn, nodeSetWithOneRespondingNode):
+    seq_no = get_seq_no(last_txn)
     helpers.general.get_txn(str(TOKEN_LEDGER_ID), seq_no)
