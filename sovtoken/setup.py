e8452513acb96f2d6d8751819f1f028563aae577
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2019-10-03 12:11:21 +0300

    INDY-2233 -- fix attrs
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- sovtoken/setup.py
+++ sovtoken/setup.py
@@ -19,7 +19,7 @@ metadata = {}
 with open(os.path.join(here, 'sovtoken', '__metadata__.py'), 'r') as f:
     exec(f.read(), metadata)
 
-tests_require = ['pytest==4.6.2', 'pytest-xdist', 'mock', 'python3-indy==1.11.1-dev-1343']
+tests_require = ['attrs==19.1.0', 'pytest==4.6.2', 'pytest-xdist', 'mock', 'python3-indy==1.11.1-dev-1343']
 
 setup(
     name=metadata['__title__'],
