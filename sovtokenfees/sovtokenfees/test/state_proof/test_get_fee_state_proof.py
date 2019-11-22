f2fa15ce304a94a371e5b83dd859b6058fe381ff
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2019-11-22 14:58:13 +0300

    bump libsovtoken; fix tests for GET_FEES state proof
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-corporation.com>

---
+++ sovtokenfees/sovtokenfees/test/state_proof/test_get_fee_state_proof.py
@@ -0,0 +1,28 @@
+import pytest
+from sovtokenfees.constants import FEE
+from sovtokenfees.test.constants import NYM_FEES_ALIAS
+
+from plenum.common.constants import STATE_PROOF
+
+from plenum.test.stasher import delay_rules
+
+from plenum.test.delayers import req_delay
+from sovtokenfees.test.helper import send_and_check_auth_rule
+
+
+@pytest.mark.skip("ST-633")
+def test_state_proof_for_get_fee(helpers, nodeSetWithIntegratedTokenPlugin,
+                                 looper, sdk_pool_handle, sdk_wallet_trustee):
+    # make sure that config ledger is BLS signed by sending a txn to config ledger
+    send_and_check_auth_rule(looper, sdk_pool_handle, sdk_wallet_trustee)
+    with delay_rules([n.clientIbStasher for n in nodeSetWithIntegratedTokenPlugin[1:]], req_delay()):
+        resp = helpers.general.do_get_fee()
+        assert resp.get(STATE_PROOF, False)
+        assert {} == resp[FEE]
+
+    fees = {NYM_FEES_ALIAS: 5}
+    helpers.general.do_set_fees(fees)
+    with delay_rules([n.clientIbStasher for n in nodeSetWithIntegratedTokenPlugin[1:]], req_delay()):
+        resp = helpers.general.do_get_fee()
+        assert resp.get(STATE_PROOF, False)
+        assert fees == resp[FEE]
