d7566899aea9308dea4a465c8189fd066d9b872e
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2019-12-21 12:02:38 +0300

    INDY-2313: Bump node version
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- sovtoken/setup.py
+++ sovtoken/setup.py
@@ -38,7 +38,7 @@ setup(
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', 'sovtoken']},
     include_package_data=True,
 
-    install_requires=['indy-node==1.12.1.dev1165'],
+    install_requires=['indy-node==1.12.1.dev1167'],
 
     setup_requires=['pytest-runner'],
     extras_require={
