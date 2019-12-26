25fe8ac976d2edc2a6b92c39a6b616a821e87503
toktar <renata.toktar@dsr-corporation.com>
2019-12-26 13:31:54 +0300

    INDY-2316: bump libindy version
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- sovtokenfees/setup.py
+++ sovtokenfees/setup.py
@@ -18,7 +18,7 @@ metadata={}
 with open(os.path.join(here, 'sovtokenfees', '__metadata__.py'), 'r') as f:
     exec(f.read(), metadata)
 
-tests_require = ['attrs==19.1.0', 'pytest==4.6.2', 'pytest-xdist', 'python3-indy==1.13.0-dev-1404']
+tests_require = ['attrs==19.1.0', 'pytest==4.6.2', 'pytest-xdist', 'python3-indy==1.13.0-dev-1420']
 
 setup(
 
