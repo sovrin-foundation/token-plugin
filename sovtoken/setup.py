fefc213544edd81820c6ae56a16c68a1ff09fc33
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2020-02-05 16:48:41 +0300

    [INDY-2326] bump node version
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- sovtoken/setup.py
+++ sovtoken/setup.py
@@ -38,7 +38,7 @@ setup(
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', 'sovtoken']},
     include_package_data=True,
 
-    install_requires=['indy-node==1.13.0.dev1196'],
+    install_requires=['indy-node==1.13.0.dev1199'],
 
     setup_requires=['pytest-runner'],
     extras_require={
